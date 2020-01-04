78e3b4adf6217711361ae048215c8e952b522bb6
softberry <emre.sakarya@softberry.de>
2020-01-04 12:54:29 +0100

    dynamic themic applied to components

---
+++ src/themes/default/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import webfontloader from "webfontloader";
+import TYPES from "../../store/action-types";
+
+import "./index.scss";
+
+export default () => {
+  const dispatch = useDispatch();
+  const webFontsConfig = {
+    google: {
+      families: ["Allerta Stencil:400", "Exo:300,600"]
+    },
+    inactive: () => {
+      dispatch({
+        type: TYPES.MESSAGES_ERROR,
+        caption: "Error loading Webfonts",
+        body: (
+          <>
+            WebFonts Could not be loaded. Please check your internet connection.
+            <br />
+            <a href="/" target="_self">
+              <strong>Reload</strong>
+            </a>
+          </>
+        ),
+        closable: true
+      });
+    },
+    classes: false
+  };
+
+  webfontloader.load(webFontsConfig);
+  return <></>;
+};
---
+++ src/themes/ocean/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import webfontloader from "webfontloader";
+import TYPES from "../../store/action-types";
+
+import "./index.scss";
+
+export default () => {
+  const dispatch = useDispatch();
+  const webFontsConfig = {
+    google: {
+      families: ["Shadows+Into+Light", "Audiowide"]
+    },
+    inactive: () => {
+      dispatch({
+        type: TYPES.MESSAGES_ERROR,
+        caption: "Error loading Webfonts",
+        body: (
+          <>
+            WebFonts Could not be loaded. Please check your internet connection.
+            <br />
+            <a href="/" target="_self">
+              <strong>Reload</strong>
+            </a>
+          </>
+        ),
+        closable: true
+      });
+    },
+    classes: false
+  };
+
+  webfontloader.load(webFontsConfig);
+  return <></>;
+};
