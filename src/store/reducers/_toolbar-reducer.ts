865ab0f4248a91e4e9361093c4baa547fdb13be1
softberry <emre.sakarya@softberry.de>
2020-01-19 02:35:49 +0100

    add worklog button moved to toolbar

--- src/store/reducers/_toolbar-reducer.ts
+++ src/store/reducers/_toolbar-reducer.ts
@@ -1,6 +1,6 @@
 import {
   IToolbarButton,
-  IToolbarButtonState
+  IToolbarButtonState,
 } from "../../__typings/interfaces";
 import TYPES from "../action-types";
 
@@ -12,9 +12,10 @@ const contacts: IToolbarButton[] = [
     disabled: false,
     hidden: false,
     clickAction: TYPES.EVENT_CREATE_CONTACT
+    
   },
   { type: "delete", disabled: true, hidden: true },
-  { type: "edit", disabled: true, hidden: true }
+  { type: "edit", disabled: true, hidden: true },
 ];
 const saveContact: IToolbarButton[] = [
   {
@@ -22,8 +23,8 @@ const saveContact: IToolbarButton[] = [
     label: "Save Contact",
     disabled: true,
     hidden: false,
-    clickAction: TYPES.EVENT_SAVE_CONTACT
-  }
+    clickAction: TYPES.EVENT_SAVE_CONTACT,
+  },
 ];
 
 const editContact: IToolbarButton[] = [
@@ -32,22 +33,22 @@ const editContact: IToolbarButton[] = [
     label: "Edit",
     disabled: false,
     hidden: false,
-    clickAction: TYPES.EVENT_EDIT_CONTACT
+    clickAction: TYPES.EVENT_EDIT_CONTACT,
   },
   {
     type: "delete",
     label: "Delete Contact",
     disabled: false,
     hidden: false,
-    clickAction: TYPES.EVENT_DELETE_CONTACT
+    clickAction: TYPES.EVENT_DELETE_CONTACT,
   },
   {
     type: "add",
     label: "New Worklog",
     disabled: false,
     hidden: false,
-    clickAction: TYPES.EVENT_CREATE_WORKLOG
-  }
+    clickAction: TYPES.EVENT_CREATE_WORKLOG,
+  },
 ];
 const calendar: IToolbarButton[] = [];
 const settings: IToolbarButton[] = [];
@@ -59,38 +60,38 @@ export default function toolBarReducer(
     case TYPES.TOOLBAR_HOME:
       return {
         ...state,
-        buttons: home
+        buttons: home,
       };
     case TYPES.TOOLBAR_CONTACTS:
       return {
         ...state,
-        buttons: contacts
+        buttons: contacts,
       };
     case TYPES.TOOLBAR_EDIT_CONTACT:
       return {
         ...state,
         buttons: editContact,
-        contact: action.contact
+        contact: action.contact,
       };
     case TYPES.TOOLBAR_SAVE_CONTACT:
       return {
         ...state,
         buttons: saveContact,
-        contact: action.contact
+        contact: action.contact,
       };
     case TYPES.TOOLBAR_CALENDAR:
       return {
         ...state,
-        buttons: calendar
+        buttons: calendar,
       };
     case TYPES.TOOLBAR_SETTINGS:
       return {
         ...state,
-        buttons: settings
+        buttons: settings,
       };
     default:
       return {
-        ...state
+        ...state,
       };
   }
 }
