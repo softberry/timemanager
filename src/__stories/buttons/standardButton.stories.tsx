946728bd019c3773ccd173b6c723c32f3d4f2996
softberry <emre.sakarya@softberry.de>
2020-02-02 19:41:56 +0100

    button varitons added into storybook

--- src/__stories/buttons/standardButton.stories.tsx
+++ src/__stories/buttons/standardButton.stories.tsx
@@ -14,10 +14,35 @@ export default {
 export const primary = () => {
   return (
     <StoryPage viewType="PrimaryView">
-      <Button icon="add" variant="primary">
+      <Button icon="add" variant="primary" actionClass="simple">
         Click Here!
       </Button>
-      <Button icon="add" variant="primary">
+      <Button icon="add" variant="primary" actionClass="negative">
+        Click Here!
+      </Button>
+      <Button icon="add" variant="primary" actionClass="positive">
+        Click Here!
+      </Button>
+      <Button icon="add" variant="primary" actionClass="error">
+        Click Here!
+      </Button>
+    </StoryPage>
+  );
+};
+
+export const primaryError = () => {
+  return (
+    <StoryPage viewType="PrimaryView">
+      <Button icon="add" variant="primary" actionClass="simple">
+        Click Here!
+      </Button>
+      <Button icon="add" variant="primary" actionClass="negative">
+        Click Here!
+      </Button>
+      <Button icon="add" variant="primary" actionClass="positive">
+        Click Here!
+      </Button>
+      <Button icon="add" variant="primary" actionClass="error">
         Click Here!
       </Button>
     </StoryPage>
@@ -27,9 +52,19 @@ export const primary = () => {
 export const secondary = () => {
   return (
     <StoryPage viewType="SecondaryView">
-      <Button icon="add" variant="secondary">
+      <Button icon="add" variant="secondary" actionClass="simple">
+        Click Here!
+      </Button>
+      <Button icon="add" variant="secondary" actionClass="negative">
+        Click Here!
+      </Button>
+      <Button icon="add" variant="secondary" actionClass="positive">
+        Click Here!
+      </Button>
+      <Button icon="add" variant="secondary" actionClass="error">
         Click Here!
       </Button>
     </StoryPage>
   );
 };
+
--- src/__ui/buttons/button/index.tsx
+++ src/__ui/buttons/button/index.tsx
@@ -13,14 +13,14 @@ const stylesMap = new Map();
 stylesMap.set(VDESIGN.DESIGN_THEME_OCEAN, themeOcean);
 stylesMap.set(VDESIGN.DESIGN_THEME_DEFAULT, themeDefault);
 
-function Button({ children, icon }: any) {
+function Button({ children, icon, actionClass = "simple" }: any) {
   const theme = useTheme();
   const styles = useThemeStyle(stylesMap);
   const view = useContext(ViewContext);
   return (
     <>
-      <div className={styles[`Btn-${theme}-basic-${view}`]}>
-        <button className={styles[`Btn-${theme}-basic-${view}`]}>
+      <div className={styles[`Btn-${theme}-${view}--${actionClass}`]}>
+        <button>
           {icon && <Icon size={ESizeIcon.SMALL}>{icon}</Icon>}
           <span>{children}</span>
         </button>
