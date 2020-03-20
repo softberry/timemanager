0f8f9ecf560c8d73cce15e87b6d4461e95877f14
softberry <emre.sakarya@softberry.de>
2020-03-20 19:48:03 +0100

    line width extended to 120 chars in prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,6 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": 2,
     "@typescript-eslint/explicit-function-return-type": 2,
     "@typescript-eslint/interface-name-prefix": 0,
-    "@typescript-eslint/no-unused-vars": 2,
+    "@typescript-eslint/no-unused-vars": 0,
   },
 };
